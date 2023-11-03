496a2c8aab2a43f4ef6a9f4d0eaee9c69063dc57
Linea-code <64187570+Linea-code@users.noreply.github.com>
2023-11-03 16:41:33 +0100

    updated api.py (not fully working)

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 import os
-import json
+
 from langchain.graphs import Neo4jGraph
 from dotenv import load_dotenv
 from utils import (
     create_vector_index,
     BaseLogger,
+    Neo4jChatMessageHistory,
 )
 from chains import (
     load_embedding_model,
@@ -12,7 +13,7 @@ from chains import (
     configure_llm_only_chain,
     configure_qa_rag_chain,
 )
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Cookie, HTTPException
 from pydantic import BaseModel
 from langchain.callbacks.base import BaseCallbackHandler
 from threading import Thread
@@ -20,9 +21,12 @@ from queue import Queue, Empty
 from collections.abc import Generator
 from sse_starlette.sse import EventSourceResponse
 from fastapi.middleware.cors import CORSMiddleware
-from langchain.memory import (
-    ConversationBufferMemory,
+import json
+import uuid
+from langchain.agents.openai_functions_agent.agent_token_buffer_memory import (
+    AgentTokenBufferMemory,
 )
+from fastapi import FastAPI, Response, Request
 
 load_dotenv(".env")
 
@@ -32,6 +36,7 @@ password = os.getenv("NEO4J_PASSWORD")
 ollama_base_url = os.getenv("OLLAMA_BASE_URL")
 embedding_model_name = os.getenv("EMBEDDING_MODEL")
 llm_name = os.getenv("LLM")
+# Remapping for Langchain Neo4j integration
 os.environ["NEO4J_URL"] = url
 
 embeddings, dimension = load_embedding_model(
@@ -40,6 +45,7 @@ embeddings, dimension = load_embedding_model(
     logger=BaseLogger(),
 )
 
+# if Neo4j is local, you can go to http://localhost:7474/ to browse the database
 neo4j_graph = Neo4jGraph(url=url, username=username, password=password)
 create_vector_index(neo4j_graph, dimension)
 
@@ -47,22 +53,47 @@ llm = load_llm(
     llm_name, logger=BaseLogger(), config={"ollama_base_url": ollama_base_url}
 )
 
-memory = ConversationBufferMemory(
-    memory_key="chat_history", return_messages=True
-)
+def set_session_id(response: Response, value: str):
+    """Helper function to set a cookie."""
+    response.set_cookie(key="session_id", value=value)
 
+def get_session_id(request: Request) -> str:
+    """Helper function to retrieve a cookie."""
+    return request.cookies.get("session_id", None)
+
+def get_or_create_session(session_id: str = Cookie(None), llm=llm):
+    """
+    Retrieve or create a session in Neo4J.
+    """
+    session_id = get_session_id(request = Request)
+    if session_id is None:
+        # Create new session
+        session_id = str(uuid.uuid4())
+        set_session_id(response= Response, value = session_id)
+        # TODO: connect to user name and time stamp (to make it very!!! unique)
+        neo4j_graph.run("CREATE (s:Session {id: $session_id, chat_history: $chat_history})", session_id=session_id, chat_history=json.dumps([]))
+        message_history = Neo4jChatMessageHistory(session_id, url, username, password )
+        MEMORY_KEY = "chat_history"
+        memory = AgentTokenBufferMemory(memory_key=MEMORY_KEY, llm=llm, chat_memory=chat_history)
+    else:
+        message_history = Neo4jChatMessageHistory(session_id, url, username, password )
+
+        MEMORY_KEY = "chat_history"
+        memory = AgentTokenBufferMemory(memory_key=MEMORY_KEY, llm=llm, chat_memory=message_history)
+
+    return session_id, memory
+
+
+session_id, memory = get_or_create_session()
 llm_chain = configure_llm_only_chain(llm)
 rag_chain = configure_qa_rag_chain(
-    llm,
-    embeddings,
-    embeddings_store_url=url,
-    username=username,
-    password=password,
-    memory=memory,
+    llm, embeddings, embeddings_store_url=url, username=username, password=password, memory= memory
 )
 
 
 class QueueCallback(BaseCallbackHandler):
+    """Callback handler for streaming LLM responses to a queue."""
+
     def __init__(self, q):
         self.q = q
 
@@ -84,6 +115,8 @@ def stream(cb, q) -> Generator:
     t.start()
 
     content = ""
+
+    # Get each new token from the queue and yield for our generator
     while True:
         try:
             next_token = q.get(True, timeout=1)
@@ -106,25 +139,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-chat_memory = {}
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 class Question(BaseModel):
     text: str
     rag: bool = False
-    session_id: str = None
+
+
+class BaseTicket(BaseModel):
+    text: str
 
 
 @app.get("/query-stream")
 def qstream(question: Question = Depends()):
-    session_id = question.session_id
-    chat_history = chat_memory.get(session_id, [])
-
     output_function = llm_chain
     if question.rag:
         output_function = rag_chain
@@ -133,7 +163,7 @@ def qstream(question: Question = Depends()):
 
     def cb():
         output_function(
-            {"question": question.text, "chat_history": chat_history},
+            {"question": question.text, "chat_history": []},
             callbacks=[QueueCallback(q)],
         )
 
@@ -146,22 +176,17 @@ def qstream(question: Question = Depends()):
 
 
 @app.get("/query")
-async def ask(question: Question = Depends()):
-    session_id = question.session_id
-    chat_history = chat_memory.get(session_id, [])
-    print("test1:", chat_memory)
-
+async def ask(question: Question = Depends(), chat_history: list = Depends(get_or_create_session)):
     output_function = llm_chain
     if question.rag:
         output_function = rag_chain
-
+    
+    chat_history[0].append(question.text)
     result = output_function(
-        {"question": question.text, "chat_history": chat_history}, callbacks=[]
+        {"question": question.text, "chat_history": chat_history[0]}, callbacks=[]
     )
 
-    chat_memory[session_id] = chat_history + [
-        {"user": question.text, "assistant": result["answer"]}
-    ]
-    # print("test2:"chat_memory)
+    # Update the session with the new chat history in Neo4J
+    neo4j_graph.run("MATCH (s:Session {id: $session_id}) SET s.chat_history = $chat_history", session_id=chat_history[1], chat_history=json.dumps(chat_history[0]))
 
-    return json.dumps({"result": result["answer"], "model": llm_name})
+    return {"result": result["output"], "model": llm_name, "session_id": chat_history[1]}
