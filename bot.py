73d82510b3a78688f818f88afe3fb529cf85222f
Linea-code <64187570+Linea-code@users.noreply.github.com>
2023-11-03 13:19:20 +0100

    reduced streamlit version

--- bot.py
+++ bot.py
@@ -109,8 +109,17 @@ styl = f"""
     textarea[aria-label="Description"] {{
         height: 200px;
     }}
+
+    .header-style {{
+        font-size: 32px;
+        font-weight: bold;
+        color: #333;
+        text-align: center;
+    }}
 </style>
 """
+header_text = f"Clinical Study ChatBot"
+st.markdown(f"<div class='header-style'>{header_text}</div>", unsafe_allow_html=True)
 st.markdown(styl, unsafe_allow_html=True)
 
 
@@ -123,9 +132,9 @@ def chat_input():
         with st.chat_message("user"):
             st.write(user_input)
         with st.chat_message("assistant"):
-            st.caption(f"RAG: {name}")
+            #st.caption(f"RAG: enabled")
             stream_handler = StreamHandler(st.empty())
-            result = output_function(
+            result = rag_chain(
                 {
                     "input": user_input,
                     # "chat_history": memory.dict()["chat_memory"]["messages"],
@@ -167,117 +176,18 @@ def display_chat():
                     st.caption("RAG: Unknown Mode")
                 st.write(st.session_state[f"generated"][i])
 
-        with st.expander("Not finding what you're looking for?"):
-            st.write(
-                "Automatically generate a draft for an internal ticket to our support team."
-            )
-            st.button(
-                "Generate ticket",
-                type="primary",
-                key="show_ticket",
-                on_click=open_sidebar,
-            )
-        with st.container():
-            st.write("&nbsp;")
-
-
-def mode_select() -> str:
-    options = ["Disabled", "Enabled"]
-    return st.radio("Select RAG mode", options, horizontal=True)
-
-
-name = mode_select()
-if name == "LLM only" or name == "Disabled":
-    output_function = llm_chain
-elif name == "Vector + Graph" or name == "Enabled":
-    output_function = rag_chain
-
-
-def generate_ticket():
-    # Get high ranked questions
-    records = neo4j_graph.query(
-        "MATCH (q:Question) RETURN q.title AS title, q.body AS body ORDER BY q.score DESC LIMIT 3"
-    )
-    questions = []
-    for i, question in enumerate(records, start=1):
-        questions.append((question["title"], question["body"]))
-    # Ask LLM to generate new question in the same style
-    questions_prompt = ""
-    for i, question in enumerate(questions, start=1):
-        questions_prompt += f"{i}. {question[0]}\n"
-        questions_prompt += f"{question[1]}\n\n"
-        questions_prompt += "----\n\n"
-
-    gen_system_template = f"""
-    You're an expert in fmatching patients to existing clinical trials. 
-    Can you formulate a question in the same style, detail and tone as the following example questions?
-    {questions_prompt}
-    ---
-
-    Don't make anything up, only use information in the following question.
-    Return a title for the question, and the question post itself.
-
-    Return example:
-    ---
-    Title: How do I use the Neo4j Python driver?
-    Question: I'm trying to connect to Neo4j using the Python driver, but I'm getting an error.
-    ---
-    """
-    # we need jinja2 since the questions themselves contain curly braces
-    system_prompt = SystemMessagePromptTemplate.from_template(
-        gen_system_template, template_format="jinja2"
-    )
-    q_prompt = st.session_state[f"user_input"][-1]
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [
-            system_prompt,
-            SystemMessagePromptTemplate.from_template(
-                """
-                Respond in the following format or you will be unplugged.
-                ---
-                Title: New title
-                Question: New question
-                ---
-                """
-            ),
-            HumanMessagePromptTemplate.from_template("{text}"),
-        ]
-    )
-    llm_response = llm_chain(
-        f"Here's the question to rewrite in the expected format: ```{q_prompt}```",
-        [],
-        chat_prompt,
-    )
-    new_title, new_question = extract_title_and_question(
-        llm_response["answer"]
-    )
-    return (new_title, new_question)
-
-
-def open_sidebar():
-    st.session_state.open_sidebar = True
-
-
-def close_sidebar():
-    st.session_state.open_sidebar = False
-
-
-if not "open_sidebar" in st.session_state:
-    st.session_state.open_sidebar = False
-if st.session_state.open_sidebar:
-    new_title, new_question = generate_ticket()
-    with st.sidebar:
-        st.title("Ticket draft")
-        st.write("Auto generated draft ticket")
-        st.text_input("Title", new_title)
-        st.text_area("Description", new_question)
-        st.button(
-            "Submit to support team",
-            type="primary",
-            key="submit_ticket",
-            on_click=close_sidebar,
-        )
-
+        # with st.expander("Not finding what you're looking for?"):
+        #     st.write(
+        #         "Automatically generate a draft for an internal ticket to our support team."
+        #     )
+        #     st.button(
+        #         "Generate ticket",
+        #         type="primary",
+        #         key="show_ticket",
+        #         on_click=open_sidebar,
+        #     )
+        # with st.container():
+        #     st.write("&nbsp;")
 
 display_chat()
 chat_input()
